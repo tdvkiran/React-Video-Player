54d16d296d2c12267620f7f6b8547c5475d061c0
Kiran Tadisetti <kiran.tadisetti@hexagon.com>
2020-08-09 17:55:37 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import Player from "./Player";
+import "./App.css";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content-wrap">
+        <header className="App-header">
+          <h3>Welcome to Video Player!!</h3>
+        </header>
+        <Player />
+      </div>
+
+      <Footer/>
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Footer.css"
+
+const Footer =props=>{
+
+    return (
+        <div className="footer">
+        <h3>© Kiran Tadisetti</h3>
+        </div>
+    )
+}
+
+
+export default Footer;
