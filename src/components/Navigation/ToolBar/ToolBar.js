7dc7dffd7ab2640562297ff8c9c676c0a8cf0978
tdvkiran <tdvkiran11@gmail.com>
2020-08-16 11:32:05 +0530

    footer styling

--- src/components/Navigation/ToolBar/ToolBar.js
+++ src/components/Navigation/ToolBar/ToolBar.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import "./ToolBar.css"
+import React from "react";
+import "./ToolBar.css";
 
-const ToolBar =props=>{
-
-    return (
-        <div className="ToolBar">
-          <h3>Welcome to Video Player!!</h3>
-        </div>
-    )
-}
-
+const ToolBar = (props) => {
+  return (
+    <div className="ToolBar">
+      <h3>Welcome to Video Player!!</h3>
+    </div>
+  );
+};
 
 export default ToolBar;
